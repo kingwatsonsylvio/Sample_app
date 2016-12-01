15f9456d3e516a310a088a288ebae89d1bfc871e
kingwatson <sylvio.watsondeveloppement@gmail.com>
2016-12-01 21:25:07 +0100

    Fin du layout et des routages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'pages/home'
 
   get 'pages/contact'
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   get 'pages/help'
+
+  get '/signup',  :to => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
